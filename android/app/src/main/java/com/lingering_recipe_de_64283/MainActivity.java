8d4d9f3e148e67639a5e6e468d2bb351dc5f5f03
Crowdbotics <engineering@crowdbotics.com>
2023-02-18 00:14:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lingering_recipe_de_64283/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lingering_recipe_de_64283;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "lingering_recipe_de_64283";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-recipe-de-64283",
+  url: "https://lingering-recipe-de-64283.botics.co",
+  api: "https://lingering-recipe-de-64283.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
